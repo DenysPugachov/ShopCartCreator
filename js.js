2611fda8998307bf7a2f88bb7dd3db8a22c0ada4
Denys <pugachovdenys@gmail.com>
2020-08-25 10:38:26 +0300

    add Look template

--- js.js
+++ js.js
@@ -5,42 +5,83 @@ const inputFront = document.querySelector("#Front");
 const inputBack = document.querySelector("#Back");
 const inputNew_price = document.querySelector("#New_price");
 const inputOld_price = document.querySelector("#Old_price");
+const inputNew_price2 = document.querySelector("#New_price_2");
+const inputOld_price2 = document.querySelector("#Old_price_2");
 const cartTempHtml = document.createElement("div");
 
 
 
-function generateHTML(logo, front, back, oldPrice, newPrice) {
-    cartTempHtml.innerHTML = `<div class="cont">
+
+
+function generateHTML(logo, front, back, oldPrice, newPrice, oldPrice_2, newPrice_2) {
+    const htmlTemplFirst = `<div class="cont">
+        <div class="item logo">
+        <img
+        src="${logo}"
+        alt=""
+        />
+        </div>
+        <div class="item front">
+        <img
+        src="${front}"
+        alt=""
+        />
+        </div>
+        <div class="item back">
+        <img
+        src="${back}"
+        alt=""
+        />
+        </div>
+        <div class="item price">
+        <p>
+        <span class="price__old">${oldPrice}</span> <br />
+        <span class="price__new">${newPrice}</span>
+        </p>
+        </div>
+        </div> `;
+
+
+    const htmlTemplLook = `
+        <div class="cont cont_look">
     <div class="item logo">
-    <img
-    src="${logo}"
-    alt=""
-    />
+        <img src="${logo}"
+            alt=""
+        />
     </div>
     <div class="item front">
-    <img
-    src="${front}"
-    alt=""
-    />
+        <img
+            src="${front}"
+            alt=""
+        />
     </div>
     <div class="item back">
-    <img
-    src="${back}"
-    alt=""
-    />
+        <img
+            src="${back}"
+            alt=""
+        />
     </div>
-    <div class="item price">
+    <div class="item price price-1">
+        <p>
+            <span class="price__old">${oldPrice}</span> <br />
+            <span class="price__new">${newPrice}</span>
+        </p>
+    </div>
+    <div class="item price price-2">
     <p>
-    <span class="price__old">${oldPrice}</span> <br />
-    <span class="price__new">${newPrice}</span>
-    </p>
+    <span class="price__old">${oldPrice_2}</span> <br />
+    <span class="price__new">${newPrice_2}</span>
+</p>
     </div>
-    </div> `
+</div>
+
+        `
+    cartTempHtml.innerHTML = htmlTemplLook;
 }
 
 
 renderBtn.addEventListener("click", () => {
-    generateHTML(inputLogo.value, inputFront.value, inputBack.value, inputOld_price.value, inputNew_price.value)
+    generateHTML(inputLogo.value, inputFront.value, inputBack.value, inputOld_price.value, inputNew_price.value, inputOld_price2.value, inputNew_price2.value);
 
     cartPlaceHolder.append(cartTempHtml);
 })
